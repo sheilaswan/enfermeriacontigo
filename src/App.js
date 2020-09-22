b41160e299e4ac77057f92fcd93a6165e76561f2
devendr4 <angel.emo.altuve@gmail.com>
2020-09-22 14:19:51 -0400

    configuracion inicial react y lite server

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Header } from "./components/HeaderComponent";
+
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
